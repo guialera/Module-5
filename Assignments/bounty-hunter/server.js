019fa9bec4a205ab825107d033041a5abaf8a965
Alex Ramirez <guialera@gmail.com>
2021-02-11 18:58:24 -0800

    Committing an updated completed Bounty Hunter assignment 2/11/2021

--- Assignments/bounty-hunter/server.js
+++ Assignments/bounty-hunter/server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const {v4: uuid} = require("uuid")
+const { v4: uuid } = require("uuid")
 
 app.use(express.json())
 
@@ -12,18 +12,57 @@ const bounties = [
         bountyAmount: 5000,
         type: "Jedi",
         _id: uuid()
+    },
+
+    {
+        firstName: "Darth",
+        lastName: "Maul",
+        living: true,
+        bountyAmount: 2000,
+        type: "Sith",
+        _id: uuid()
     }
 
 ]
+
+//Get All
 
 app.get("/bounties", (req, res) => {
     res.send(bounties)
 })
 
-app.post("/bounties", (req, res)=>{
+//Get One
+
+app.get("/bounties/:bountyId", (req, res) => {
+    const bountyId = req.params.bountyId
+    let foundBounty = bounties.find(each => each._id === bountyId)
+    res.send(foundBounty)
+})
+
+//Post One
+
+app.post("/bounties", (req, res) => {
     req.body._id = uuid()
     bounties.push(req.body)
     res.send(`Submitted ${req.body.firstName} in bounties!`)
+})
+
+//Put One
+
+app.put("/bounties/:bountyId", (req, res) => {
+    const bountyId = req.params.bountyId
+    let editBounty = bounties.find(each => each._id === bountyId)
+    let newBounty = Object.assign(editBounty, req.body)
+    res.send(newBounty)
+})
+
+//Delete One
+
+app.delete("/bounties/:bountyId", (req, res) => {
+    const bountyId = req.params.bountyId
+    let index = bounties.findIndex((each) => (each._id === bountyId))
+    bounties.splice(index, 1)
+    res.send(bounties)
 })
 
 app.listen(9000, () => {
